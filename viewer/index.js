49a03c7b6914927e6128bb2bfd8ec3c56eaf422c
Paul Sonnentag <paul.sonnentag@googlemail.com>
2016-12-14 23:52:28 +0100

    Initial commit

---
+++ viewer/index.js
@@ -0,0 +1,30 @@
+const html = require('choo/html')
+const choo = require('choo')
+const app = choo()
+
+app.model({
+  state: { title: 'Not quite set yet' },
+  reducers: {
+    update: function (state, data) {
+      return { title: data }
+    }
+  }
+})
+
+function mainView (state, prev, send) {
+  return html`
+    <main>
+      <h1>Title: ${state.title}</h1>
+      <input type="text" oninput=${update}>
+    </main>
+  `
+
+  function update (e) {
+    send('update', e.target.value)
+  }
+}
+
+app.router(['/', mainView])
+
+const dom = app.start()
+document.body.appendChild(dom)
