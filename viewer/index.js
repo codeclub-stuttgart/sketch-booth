3f8b76025f09abeedf804eafccb54a9a235d9dfe
Paul Sonnentag <paul.sonnentag@googlemail.com>
2016-12-15 14:05:31 +0100

    Make sketches selectable

--- viewer/index.js
+++ viewer/index.js
@@ -3,46 +3,55 @@ const update = require('immutability-helper')
 const chokidar = require('chokidar')
 const html = require('choo/html')
 const choo = require('choo')
+const uid = require('uid')
 const classNames = require('classnames')
 
 const app = choo()
 
 app.model({
   state: {
+    selectedSketchId: null,
     sketches: {}
   },
 
   effects: {
-    loadFile: (state, { path }, send) => {
-      const img = new Image()
+    loadSketch: (state, { path }, send) => {
+      const id = uid()
 
       // this is required to handle files which already exist and are not currently uploaded
+      const img = new Image()
       img.src = path
-      img.onload = () => send('setFileStatus', { path, loading: false }, _.noop)
+      img.onload = () => send('setSketchStatus', { id, loading: false }, _.noop)
 
-      send('addFile', { path }, _.noop)
+      send('addSketch', { path, id, loading: true }, _.noop)
     }
   },
 
   reducers: {
-    addFile: (state, { path }) => {
+    selectSketch: (state, { id }) => {
       return update(state, {
-        sketches: { [path]: { $set: { path, loading: true } } }
+        selectedSketchId: { $set: id }
+      });
+    },
+
+    addSketch: (state, { path, id, loading }) => {
+      return update(state, {
+        sketches: { [id]: { $set: { path, id, loading } } }
       })
     },
 
-    removeFile: (state, { path }) => {
-      const newSketches = _.omitBy(state.sketches, (sketch) => sketch.path !== path)
+    removeSketch: (state, { id }) => {
+      const sketches = _.omit(state.sketches, [id])
 
       return update(state, {
-        sketches: { $set: newSketches }
+        sketches: { $set: sketches }
       })
     },
 
-    setFileStatus: (state, { path, loading }) => {
+    setSketchStatus: (state, { id, loading }) => {
       return update(state, {
         sketches: {
-          [path]: { loading: { $set: loading } }
+          [id]: { loading: { $set: loading } }
         }
       })
     }
@@ -51,22 +60,39 @@ app.model({
   subscriptions: {
     fileWatcher: (send) => {
       chokidar.watch('sketches', { ignored: /[\/\\]\./ })
-        .on('add', (path) => send('loadFile', { path }, _.noop))
-        .on('change', _.debounce((path) => send('setFileStatus', { path, loading: false }, _.noop), 1000))
-        .on('unlink', (path) => send('removeFile', { path }, _.noop));
+        .on('add', (path) => send('loadSketch', { path }, _.noop))
+        .on('change', _.debounce((path) => send('setSketchStatus', { path, loading: false }, _.noop), 1000))
+        .on('unlink', (path) => send('removeSketch', { path }, _.noop));
     }
   }
 })
 
 function mainView(state, prev, send) {
-  const sketches = _(state.sketches)
+  const { sketches, selectedSketchId } = state
+
+  let previewHTML
+  const sketchesHTML = _(state.sketches)
     .sortBy('path')
     .reverse()
+    .map(({ id, path, loading }) => (
+      sketchView({
+        id, path, loading,
+        selected: id === selectedSketchId,
+        onSelect: (id) => send('selectSketch', {id })
+      })
+    ))
     .value()
+
+  if (selectedSketchId) {
+    previewHTML = html`<img src=${sketches[selectedSketchId].path}>`
+  }
 
   return html`
     <main>
-      ${sketchListView({ sketches })}
+      ${previewHTML}
+      <div class="sketches">
+        ${sketchesHTML}
+      </div>
     </main>
   `
 
@@ -75,34 +101,31 @@ function mainView(state, prev, send) {
   }
 }
 
-function sketchListView({ sketches }) {
-  console.log('rerender sketchlist')
+function sketchView({
+  id, path, loading, selected,
+  onSelect
+}) {
+  let contentHTML
 
-  const sketchItemsHTML = _.map(sketches, (sketch) => {
-    let contentHTML;
-
-    if (sketch.loading) {
-      contentHTML = html`
-        <div class="spinner">
-          <div class="bounce1"></div>
-          <div class="bounce2"></div>
-          <div class="bounce3"></div>
-        </div>
-      `
-    } else {
-      contentHTML = html`<img src="${sketch.path}" draggable>`
-    }
-
-    return html`
-      <div class="sketch">
-        ${contentHTML}
+  if (loading) {
+    contentHTML = html`
+      <div class="spinner">
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
       </div>
     `
+  } else {
+    contentHTML = html`<img src="${path}" draggable>`
+  }
+
+  const classes = classNames('sketch', {
+    'is-selected': selected
   })
 
   return html`
-    <div class="sketches">
-      ${sketchItemsHTML}
+    <div class="${classes}" onclick=${() => onSelect(id)}>
+      ${contentHTML}
     </div>
   `
 }
