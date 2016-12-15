8955ef5fea1084f4733dae2a7c3a636ea46d72cf
Paul Sonnentag <paul.sonnentag@googlemail.com>
2016-12-15 01:01:25 +0100

    Display uploaded sketches in viewer

--- viewer/index.js
+++ viewer/index.js
@@ -1,33 +1,65 @@
+const _ = require('lodash')
+const update = require('immutability-helper')
+const chokidar = require('chokidar')
 const html = require('choo/html')
 const choo = require('choo')
+
 const app = choo()
 
 app.model({
-  state: { title: 'Not quite set yet' },
+  state: {
+    sketches: []
+  },
   reducers: {
-    update: function (state, data) {
-      return { title: data }
+    addFile: (state, {sketch}) => {
+      return update(state, {
+        sketches: {$push: [sketch]}
+      })
+    },
+
+    removeFile: (state, {sketch}) => {
+      const newSketches = _.filter(state.sketches, (otherSketch) => otherSketch !== sketch)
+
+      return update(state, {
+        sketches: {$set: newSketches}
+      })
     }
   },
 
-  subscriptions: [
-
-    
-
-  ]
+  subscriptions: {
+    fileWatcher: (send) => {
+      chokidar.watch('sketches', {ignored: /[\/\\]\./})
+        .on('add', (file) => send('addFile', { sketch: file }, _.noop))
+        .on('unlink', (file) => send('removeFile', { sketch: file }, _.noop));
+    }
+  }
 })
 
-function mainView (state, prev, send) {
+function mainView(state, prev, send) {
   return html`
     <main>
-      <h1>Title: ${state.title}</h1>
-      <input type="text" oninput=${update}>
+      <h1>Sketches</h1>
+      
+      ${sketchListView({ sketches: state.sketches })}
+      
     </main>
   `
 
-  function update (e) {
+  function update(e) {
     send('update', e.target.value)
   }
+}
+
+function sketchListView ({ sketches }) {
+  const sketchItemsHTML = _.map(sketches, (sketch) => html`
+    <li><img src="${sketch}"></li>
+  `)
+
+  return html`
+    <ul>
+      ${sketchItemsHTML}
+    </ul>
+  `
 }
 
 app.router(['/', mainView])
